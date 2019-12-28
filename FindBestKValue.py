a09dd61b5a4b3e92b74b95659c67d63afdd4962c
16373576 <c.kennedy35@nuigalway.ie>
2019-12-28 17:31:08 +0000

    ran findBestK value for normalized data

--- FindBestKValue.py
+++ FindBestKValue.py
@@ -3,23 +3,38 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('C:/Users/caire/OneDrive/Documents/forth yr semester 1/Final Year Project/HTMLTagsFrequency.csv',
-                 header=0, delimiter=",")
-df = df.sample(frac=1)
-train_split = int((len(df) * 2) / 3)
-training = df.values[0:train_split]
-test = df.values[train_split - 1:]
+# df = pd.read_csv('C:/Users/caire/OneDrive/Documents/forth yr semester 1/Final Year Project/HTMLTagsFrequency.csv',
+#                  header=0, delimiter=",")
+df = pd.read_csv(
+    'C:/Users/caire/OneDrive/Documents/forth yr semester 1/Final Year Project/HTMLTagsIndividualArticlesNormalized.csv',
+    header=0, delimiter=",")
+
+data_before_feature_sel = df.values[:, :305]
+cor = df.corr(method='pearson')
+cor_target = abs(cor["Reliability"])
+relevant_features = cor_target[cor_target > 0.35]
+print(relevant_features)
+data = df[[relevant_features.index[0], relevant_features.index[1], relevant_features.index[2],
+           relevant_features.index[3], relevant_features.index[4], 'Reliability']]
+
+data = data.sample(frac=1)
+train_split = int((len(data) * 2) / 3)
+training = data.values[0:train_split]
+test = data.values[train_split - 1:]
 
 error = []
 # Calculating error for K values between 1 and 40
-for i in range(1, 40):
+for i in range(1, 200, 10):
     knn = KNeighborsClassifier(n_neighbors=i)
-    knn.fit(training[:, :305], training[:, 307])
-    predictions = knn.predict(test[:, :305])
+    x = training[:, :4]
+    y = training[:, 5]
+    y = y.astype('int')
+    knn.fit(x, y)
+    predictions = knn.predict(test[:, :4])
     error.append(np.mean(predictions != test[:, -1]))
 
 plt.figure(figsize=(12, 6))
-plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',
+plt.plot(range(1, 200, 10), error, color='red', linestyle='dashed', marker='o',
          markerfacecolor='blue', markersize=10)
 plt.title('Error Rate K Value')
 plt.xlabel('K Value')
